288a727d15ad18bf9a27818a80b870de8228e2bf
ajhgarcia <42371495+ajhgarcia@users.noreply.github.com>
2018-08-16 09:25:14 +0100

    Add project files.

---
+++ WebApplication3/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication3.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Centro" runat="server">
+    <h4>Productos:</h4>
+
+    <asp:TextBox ID="txtBusqueda" runat="server" placeholder="escribe aquí que quieres buscar"></asp:TextBox>
+    <asp:Button ID="botonCargar" runat="server" Text="Cargar" CssClass="btn btn-primary" OnClick="botonCargar_Click" />
+
+    <asp:GridView ID="tablaProductos" runat="server" CssClass="table" AutoGenerateColumns="false">
+        <Columns>
+            <asp:ImageField DataImageUrlField="url_imagen" ControlStyle-Width="180" />
+            <asp:BoundField DataField="marca" HeaderText="Marca" />
+            <asp:BoundField DataField="nombre" HeaderText="Producto" />
+            <asp:BoundField DataField="precio" HeaderText="Oferta para ti" />
+
+        </Columns>
+
+    </asp:GridView>
+
+</asp:Content>
---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void botonCargar_Click(object sender, EventArgs e)
+        {
+            using( var connexion =  new DB.db_albepruebae3Entities())
+            {
+                var productos = connexion.productos.ToList();
+                if (txtBusqueda.Text.Length > 0)
+                {
+                    var stringa = txtBusqueda.Text.Trim().ToLower();
+                    productos = productos.Where(t=>
+                        t.marca.ToLower().Contains(stringa)
+                        ||
+                        t.nombre.ToLower().Contains(stringa)
+                    
+                    ).ToList();
+                }
+
+
+                tablaProductos.DataSource = productos;
+                tablaProductos.DataBind();
+            }
+        }
+    }
+}
